3713840d47223fb700c010d4de01b24ac9c4bb2d
dalryo <daltonryoung@gmail.com>
2025-02-17 16:14:36 -0500

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ async function run() {
 
   // Write your prompt here
   const prompt =
-    "generate incredibly controversial and insightful discourse, use gen z slang like bruh, bro, or girl using acronyms like LMAOO, lol, wtf; it should be designed to rage bait, related to sports, politics, donald trump, pop culture, a list celebrities, anything you see fit; it should not be vague and should be unique; under 280 characters and should be plain text, you can use emojis";
+    "generate incredibly controversial and insightful discourse, use gen z slang like bruh, bro, or girl using acronyms like LMAOO, lol, wtf; post only one statement at a time, use all lowercase and refrain from using capital letters at all; it should be designed to rage bait, related to sports, politics, donald trump, pop culture, a list celebrities, anything you see fit; it should not be vague and should be unique; under 280 characters and should be plain text, you can use emojis";
 
   const result = await model.generateContent(prompt);
   const response = await result.response;
